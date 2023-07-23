342a39b6116223886786b315e8e303fd9fa7cf5b
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-07-24 01:54:59 +0330

    added resume file

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,8 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="MohammadhosseinAsadi.pdf"
+                  download
                 >
                   Resume
                 </a>
