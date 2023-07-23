3a287ebb911dc8765e5fb211007846d25a2a45be
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-07-24 02:20:06 +0330

    Fix download resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href="MohammadhosseinAsadi.pdf"
-                  download
+                  rel="noopener noreferrer"
                 >
                   Resume
                 </a>
